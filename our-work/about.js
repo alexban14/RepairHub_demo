312070fc65e8a3c75305151bb84091e82fd99a6b
Ban <alexbanut10@gmail.com>
2022-09-21 17:28:42 +0300

    implemented an weather API which displays current weather in an widget in the about us page

--- our-work/about.js
+++ our-work/about.js
@@ -1,21 +1,60 @@
-function httpGetAsync(url, callback) {
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
-        callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", url, true); // true for asynchronous
-    xmlHttp.send(null);
-}
+const tRow = document.querySelector(".active-row")
 
-let url = "https://holidays.abstractapi.com/v1/?api_key=aeb551d65350431c93b2fc32843ae3dd&country=RO&year=2022&month=09"
+const tdOne = document.querySelector("#tdOne")
+const tdTwo = document.querySelector("#tdTwo")
+const tdThree = document.querySelector("#tdThree")
+const tdFour = document.querySelector("#tdFour")
+const tdFive = document.querySelector("#tdFive")
 
-httpGetAsync(url)
+// const tDatas = [tdOne, tdTwo, tdThree, tdFour, tdFive];
+
+const apiKey = "6f575d2559a3adcc09315aa73de39e77";
+const cityName = 'Sibiu';
+// const lonSibiu = 26.10253839;
+// const latSibiu = 44.4267674;
+
+// const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latSibiu}&lon=${lonSibiu}&appid=${apiKey}&units=imperial`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
 
 fetch(url)
-    .then(Response => {
-        if (!Response.ok) {
-            throw new Error(`HTTP error: ${Response.status}`);
-        }
-        
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        displayData(data);
     })
+    .catch((err) => {
+        console.log('Error fetching data', err);
+        displayErr(err);
+    })
+
+function displayData(data) {
+    const {main, clouds} = data;
+    console.log( main.temp, main.feels_like, main.temp_min, main.humidity);
+        
+    const celsiusTemp = Math.round(((main.temp - 32) * 5/9) * 10) / 10;
+    console.log(celsiusTemp);
+    tdOne.textContent = `${celsiusTemp} grade celsius`;
+
+    const celsiusFeelsLike = Math.round(((main.feels_like - 32) * 5/9) * 10) / 10;
+    console.log(celsiusFeelsLike);
+    tdTwo.textContent = `${celsiusFeelsLike} grade celsius`;
+
+    const celsiusMinTemp = Math.round(((main.temp_min - 32) * 5/9) * 10) / 10;
+    console.log(celsiusMinTemp);
+    tdThree.textContent = `${celsiusMinTemp} grade celsius`;
+
+    console.log(main.humidity);
+    tdFour.textContent = `${main.humidity}%`;
+
+    console.log(clouds.all)
+    tdFive.textContent = `${clouds.all}%`
+}
+
+function displayErr(err) {
+    tdOne.textContent = `Nu putem ajunge la starea vremii din server.`;
+}
+
+
+// °C = (°F - 32) × 5/9;
+// const tempCelsius = (data.main.temp - 32) * 5/9;
+// tdOne.textContent = data.main.temp;
